e1853bf9b07b0457bc4ae156783c8e745c5293e5
Basch <ido.basch@sap.com>
2017-03-25 16:45:44 +0300

    Initial commit

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const API_KEY = '6e91683f4b732ea7d53b40747c0dd0d4';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
